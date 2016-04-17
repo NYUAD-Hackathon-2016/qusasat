3236966defefcb0c835dc13be990fc95f03440c0
muhammedAlsayadi <muhammedtraveller@gmail.com>
2016-04-17 13:02:17 +0400

    Update Snippet.java

--- postprocessing/src/Snippet.java
+++ postprocessing/src/Snippet.java
@@ -1,3 +1,5 @@
+package com.qusasat.snippet;
+
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,144 +9,168 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class Snippet 
+public class Snippet
 {
-	String str;
-	String ar[];
+    String mText;
+    String mType;
+    String mTag;
+    char mLastChar;
+    String mNiceLastChar; // Programmers are funny!! We just can't help it :3
+
+    List<String> mWords;
+    List<Snippet> mSnippets;
+
+    public static void main(String[] args)
+    {
+        Snippet s = new Snippet();
+        s.mWords = new ArrayList<>();
+
+        // Get the training data from a text file
+        s.readDataFile();
+
+        // Chop words into Snippet objects
+        s.snippetize();
+
+        // Create the CRF++-compliant input file
+        s.createCRFFile();
+
+    } // end main
+
+    public void readDataFile()
+    {
+        try
+        {
+            String lineHolder;
+            String arrayHolder[];
+            
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("train.txt"));
+
+            while ((lineHolder = bufferedReader.readLine()) != null)
+            {
+                arrayHolder = lineHolder.split(" ");
+                for(int i = 0; i < arrayHolder.length; i++)
+                {
+                    // Remove punctuations
+                    arrayHolder[i] = arrayHolder[i].replaceAll("[^\\'|>&<}AbptvjHxd*rzs$SDTZEg_fqklmnhwYyFNKaui~o]", "");
+
+                    // Replace ' with Q for compatibility with CRF++
+                    arrayHolder[i] = arrayHolder[i].replaceAll("'", "Q");
+
+                    // Remove double and trible spaces
+                    arrayHolder[i] = arrayHolder[i].replaceAll("  ", " ");
+                    arrayHolder[i] = arrayHolder[i].replaceAll("   ", " ");
+
+                    if(arrayHolder[i].length() > 0)
+                    {
+                        mWords.add(arrayHolder[i]);
+
+                    } // end if
+
+                } // end for
+
+            } // end while
+
+            bufferedReader.close();
+
+        } // end try
+
+        catch (IOException e)
+        {
+            System.out.println("File read error");
+
+        } // end catch
+
+    } // end method readDataFile
+
+    public void snippetize()
+    {
+        mSnippets = new ArrayList<Snippet>();
+
+        for (int j = 0; j < mWords.size(); j++)
+        {
+
+            // Adding spaces after non-connected letters
+            String regEx = "(Q|\\||>|&|}|A|d|\\*|r|z|W|Y)";
+            String spacedTextHolder = mWords.get(j).replaceAll(regEx, "$1 ");
+
+            String splittedTextHolder[] = spacedTextHolder.split(" ");
+
+            Snippet currentSnippet = null;
+
+            for (int i = 0; i < splittedTextHolder.length; i++)
+            {
+                // Construct the current Snippet object
+                currentSnippet = new Snippet();
+
+                currentSnippet.mText = splittedTextHolder[i];
+                currentSnippet.mType = "let";
+                currentSnippet.mLastChar = splittedTextHolder[i]
+                        .charAt(splittedTextHolder[i].length() - 1);
+                currentSnippet.mNiceLastChar =isLastCharNice(splittedTextHolder[i]);
+
+                if (i == 0)
+                {
+                    currentSnippet.mTag = "B";
+
+                } // end if
+                else
+                {
+                    currentSnippet.mTag = "I";
+
+                } // end else
+
+                mSnippets.add(currentSnippet);
+
+            } // end inner for
+
+        } // end outer for
+
+    } // end method snippetize
 
 
-	String text;
-	String type;
-	String tag;
-	char lastChar;
-	String niceLastChar;
+    public void createCRFFile()
+    {
+        try
+        {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("trainCRF.txt"));
 
-	List<String> words;
-	List<Snippet> snippets;
+            for (int i = 0; i < mSnippets.size(); i++)
+            {
+                bufferedWriter.write(mSnippets.get(i).mText +"\t"+ mSnippets.get(i).mLastChar +"\t" +
+                        mSnippets.get(i).mNiceLastChar +"\t" + mSnippets.get(i).mTag +"\n");
 
-	public static void main(String [ ] args)
-	{
-		Snippet s = new Snippet();	
-		s.words=new ArrayList<>();
-		s.readOriginal();
-		
-		s.snipetize(s.words);
-		s.OutputFile();
+            } // end for
 
-	}
+            bufferedWriter.close();
 
-	public  void readOriginal()
-	{
-		try
-		{
-			BufferedReader in = new BufferedReader(new FileReader("train.txt"));
-		
+        } // end try
 
-			while ((str = in.readLine()) != null) {
-				ar=str.split(" ");
-				for(int i=0;i<ar.length;i++)
-				{
-					ar[i] = ar[i].replaceAll("[^\\'|>&<}AbptvjHxd*rzs$SDTZEg_fqklmnhwYyFNKaui~o]", "");
-					ar[i] = ar[i].replaceAll("'", "Q");
-					ar[i] = ar[i].replaceAll("  ", " ");
-					ar[i] = ar[i].replaceAll("   ", " ");
-					if(ar[i].length()>0)
-					{
-						words.add(ar[i]);
+        catch (IOException e)
+        {
+            System.out.println("Output file error!!");
 
-					}
+        } // end catch
 
+    } // end method createCRFFile
 
-				}
-			}
-			in.close();
-		} catch (IOException e) {
-			System.out.println("File Read Error");
-		}
-
-	}
-
-	public void snipetize (List<String> wordList)
-	{
-		snippets = new ArrayList<Snippet>();
-
-		for (int j = 0; j < wordList.size(); j++) 
-		{
-		
-			String regEx = "(Q|\\||>|&|}|A|d|\\*|r|z|W|Y)";
-			String snipittedWord = wordList.get(j).replaceAll(regEx, "$1 ");
-			
-			String splittedWords[] = snipittedWord.split(" ");
-			
-		
-			
-			Snippet currentSnippet = null;
-			
-			for (int i = 0; i < splittedWords.length; i++)
-			{
-				currentSnippet = new Snippet();
-		
-				currentSnippet.text = splittedWords[i];
-				currentSnippet.type = "let";
-				currentSnippet.lastChar = splittedWords[i]
-						.charAt(splittedWords[i].length() - 1);
-				currentSnippet.niceLastChar=isLastCharNice(splittedWords[i]);
-
-				if (i == 0)
-					currentSnippet.tag = "B";
-				else
-					currentSnippet.tag = "I";
-				
-
-				snippets.add(currentSnippet);
-				
-			}
-			
-	
-		}
-	}
-
-
-	public  void OutputFile() 
-	{
-		try 
-		{
-			BufferedWriter out = new BufferedWriter(new FileWriter("trainCRF.txt"));
-			
-			
-			for (int i = 0; i < snippets.size(); i++) 
-			{
-				out.write(snippets.get(i).text+"\t"+snippets.get(i).lastChar+"\t" +snippets.get(i).niceLastChar +"\t" +snippets.get(i).tag+"\n");
-			}
-			
-			
-			out.close();
-		} catch (IOException e) {}
-	}
-	
 	/*
-	 * ****** Helper funcitons
+	 * ****** Helper methods *******
 	 */
-	
-	public String isLastCharNice(String snippetText)
-	{
-		
-		if(snippetText.endsWith("\\")||snippetText.endsWith("|")||snippetText.endsWith(">")||
-				snippetText.endsWith("<")||snippetText.endsWith("}")||snippetText.endsWith("A")||
-				snippetText.endsWith("*")||snippetText.endsWith("r")||snippetText.endsWith("z")||
-				snippetText.endsWith("w")||snippetText.endsWith("Y"))
-		{
-			return "F";
-			
-		}
-		return "T";
-	
-		
-		
-		
-	}
-	
-}
 
+    public String isLastCharNice(String snippetText)
+    {
 
+        if(snippetText.endsWith("\\")||snippetText.endsWith("|")||snippetText.endsWith(">")||
+                snippetText.endsWith("<")||snippetText.endsWith("}")||snippetText.endsWith("A")||
+                snippetText.endsWith("*")||snippetText.endsWith("r")||snippetText.endsWith("z")||
+                snippetText.endsWith("w")||snippetText.endsWith("Y"))
+        {
+            return "F";
+            
+        } // end if 
+        
+        return "T";
 
+    } // end method isLastCharNice
+
+} // end class Snippet
